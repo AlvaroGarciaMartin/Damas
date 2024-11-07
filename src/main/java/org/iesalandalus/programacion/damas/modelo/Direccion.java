9d8c10940b5e4a3623afbf2f62a486b01d5a6ca3
AlvaroGarciaMartin <gam.roalva@hotmail.com>
2024-11-07 18:03:39 +0100

    Añadido mi nombre al README.md

---
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Direccion.java
@@ -0,0 +1,7 @@
+package org.iesalandalus.programacion.damas.modelo;
+
+public enum Direccion {
+    //Crear las direcciones de la dama//
+    NORESTE("Noreste"), SURESTE("Sureste"), SUROESTE("Suroeste"), NOROESTE("Noroeste");
+
+}
