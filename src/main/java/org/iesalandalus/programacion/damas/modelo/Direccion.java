b3a22708f73e96ce5ca2e3ca14babf78ed69df64
AlvaroGarciaMartin <gam.roalva@hotmail.com>
2024-11-07 18:07:01 +0100

    crear las direcciones de la dama

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Direccion.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Direccion.java
@@ -3,5 +3,14 @@ package org.iesalandalus.programacion.damas.modelo;
 public enum Direccion {
     //Crear las direcciones de la dama//
     NORESTE("Noreste"), SURESTE("Sureste"), SUROESTE("Suroeste"), NOROESTE("Noroeste");
-
+    private String cadenaAMostrar;
+    private Direccion(String mensajeAMostrar)
+    {
+        this.cadenaAMostrar=mensajeAMostrar;
+    }
+    //Crear cadenaAMostrar//
+    @Override
+    public String toString() {
+        return ordinal()+".-"+cadenaAMostrar;
+    }
 }
