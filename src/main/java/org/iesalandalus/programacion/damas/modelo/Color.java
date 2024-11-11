6f913e15a6a5121a706eeef549fbcbb7487a40db
AlvaroGarciaMartin <gam.roalva@hotmail.com>
2024-11-11 21:47:31 +0100

    crear las colores de la dama

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Color.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Color.java
@@ -1,5 +1,15 @@
 package org.iesalandalus.programacion.damas.modelo;
 
 public enum Color {
-
+    BLANCO("Blanco"),NEGRO("Negro");
+    private String cadenaAMostrar;
+    private Color(String mensajeAMostrar)
+    {
+        this.cadenaAMostrar=mensajeAMostrar;
+    }
+    //Crear cadenaAMostrar//
+    @Override
+    public String toString() {
+        return ordinal()+".-"+cadenaAMostrar;
+    }
 }
