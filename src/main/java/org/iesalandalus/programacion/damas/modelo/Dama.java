f7345beae6ee0de96355767293a9aa62888a6a1d
AlvaroGarciaMartin <gam.roalva@hotmail.com>
2024-11-15 18:27:04 +0100

    Crear Constructor para fila y columna primera ficha aleatoria

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
@@ -5,6 +5,48 @@ public class Dama {
     private Posicion posicion;
     private boolean esDamaEspecial;
 
+    //crear constructor para ficha inicial
+    public Dama(){
+       this.posicion=crearPosicionInicial();
+       setColor(Color.BLANCO);
+       this.esDamaEspecial=false;
+    }
+    //crear metodo crearPosicionInicial random para colocar primera ficha
+    private Posicion crearPosicionInicial(){
+        int columna;
+        int fila;
+        if (color == Color.BLANCO) {
+            fila=(int) (Math.random()*3)+1; //para la fila del 1 al 3 corresponde a blanca
+        }else{
+            fila=(int) (Math.random()*3)+6; //para la fila del 6 al 8 corresponde a negra
+        }
+
+        if (fila % 2 == 0) {
+            do {
+                columna = (int) (Math.random()*8)+1;
+            } while (columna % 2 != 0);
+        }else{
+            do {
+                columna = (int) (Math.random()*8)+1;
+            } while (columna % 2 == 0);
+        }
+        char letraColumna = switch (columna) {
+            case 1 -> 'a';
+            case 2 -> 'b';
+            case 3 -> 'c';
+            case 4 -> 'd';
+            case 5 -> 'e';
+            case 6 -> 'f';
+            case 7 -> 'g';
+            case 8 -> 'h';
+            default -> ' ';
+        };
+        return new Posicion(fila,letraColumna);
+
+    }
+
+
+
     public Color getColor() {
         return color;
     }
