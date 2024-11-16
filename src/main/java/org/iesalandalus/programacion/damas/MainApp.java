9ed779bc385a1a348021f4b3979422fad6c57771
AlvaroGarciaMartin <gam.roalva@hotmail.com>
2024-11-16 15:40:59 +0100

    Crear atributo ejecutarOpcion

--- src/main/java/org/iesalandalus/programacion/damas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/damas/MainApp.java
@@ -1,8 +1,37 @@
 package org.iesalandalus.programacion.damas;
+import  org.iesalandalus.programacion.damas.modelo.Direccion;
 import  org.iesalandalus.programacion.damas.modelo.Dama;
+import  org.iesalandalus.programacion.damas.modelo.Color;
+import  org.iesalandalus.programacion.damas.modelo.Posicion;
 import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class MainApp {
  private static Dama dama;
+
+ public static void main(String[] args) {
+  int opcionElegida;
+  do{
+   Consola.mostrarMenu();
+   opcionElegida=Consola.elegirOpcionMenu();
+  }while(opcionElegida!=4);
+
+
+ }
+ private static void ejecutarOpcion( int opcion){
+
+  switch (opcion){
+   case 1:
+    crearDamaPorDefecto();
+    break;
+   case 2:
+    crearDamaColor();
+    break;
+   case 3:
+    mover();
+    break;
+   case 4:
+    Consola.despedirse();
+  }
+ }
 
 }
