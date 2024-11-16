d4234afce8c0597b6e2c60e953c48f1579efa90a
AlvaroGarciaMartin <gam.roalva@hotmail.com>
2024-11-16 10:58:29 +0100

    Crear constructor para metodos y crear mostrarMenu

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -1,4 +1,20 @@
 package org.iesalandalus.programacion.damas;
 
+import  org.iesalandalus.programacion.damas.modelo.Direccion;
+import  org.iesalandalus.programacion.damas.modelo.Dama;
+import  org.iesalandalus.programacion.damas.modelo.Color;
+import  org.iesalandalus.programacion.damas.modelo.Posicion;
+
+
 public class Consola {
+    private Consola(){
+
+    }
+
+    public static void mostrarMenu(){
+       System.out.println(" 1. crear dama por defecto");
+       System.out.println(" 2. crear dama eligiendo el color");
+       System.out.println(" 3. mover");
+       System.out.println(" 4. salir");
+    }
 }
