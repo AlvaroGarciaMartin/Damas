1700f88a51ada27db994dc97efef21fef09ce7ec
AlvaroGarciaMartin <gam.roalva@hotmail.com>
2024-11-16 13:56:11 +0100

    Crear atributo clase dama

--- src/main/java/org/iesalandalus/programacion/damas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/damas/MainApp.java
@@ -1,6 +1,8 @@
 package org.iesalandalus.programacion.damas;
+import  org.iesalandalus.programacion.damas.modelo.Dama;
+import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class MainApp {
-	
-	
+ private static Dama dama;
+
 }
